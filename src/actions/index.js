2f777388fd5303071766a59299e07e6eb77523fb
john1199 <jhon091199@gmail.com>
2020-10-08 14:43:20 -0500

    solution challenge

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,9 @@ const addToCart = payload => ({
   type: 'ADD_TO_CART',
   payload,
 });
+const deleteToCart = payload => ({
+  type: 'DELETE_TO_CART',
+  payload,
+});
 
-export { addToCart };
+export { addToCart, deleteToCart }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,33 @@
 const reducer = (state, action) => {
+  const { cart } = state;
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case 'ADD_TO_CART': {
+      const itemCart = state.cart.find(items => items.id === action.payload.id);
+
+      if (itemCart) {
+        const newCart = cart.filter(
+          item => item.id !== action.payload.id
+        );
+        itemCart.count +=1;
+        newCart.push(itemCart);
+        return {
+          ...state,
+          cart: newCart,
+        };
+      }
       return {
         ...state,
-        cart: [...state.cart, action.payload]
-      }
+        cart: [...state.cart, action.payload],
+      };
+    }
+    case 'DELETE_TO_CART': 
+      return {
+        ...state,
+        cart: state.cart.filter((items) => items.id !== action.payload),
+      };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
