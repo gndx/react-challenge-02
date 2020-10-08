1fc29497f8730fad1bd0a723a8d2e3c58a32ea3f
LoreNa <lmosquerag1@gmail.com>
2020-10-07 20:39:29 -0500

    Reto 2 React Punto 2

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteItemFromCart } from '../actions';
 import '../styles/components/Checkout.styl';
 
 const Checkout = (props) => {
   const { cart } = props;
+
+  const handleDeleteItemFromCart = (itemId) => {
+    props.deleteItemFromCArt(itemId);
+  }
+  let totalprice = 0;
+  if (cart.lenght > 0){
+    for(let i=0; i<cart.length; i++){
+      totalPrice += cart[i].price;
+    }
+  }  
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>Sin Pedidos</h2>}
-        {cart.map(item => (
-          <div className="Checkout-item">
+        {cart.map((item, index) => (
+          <div className="Checkout-item" key={index}>
             <div className="Checkout-element">
               <h4>{item.title}</h4>
               <span>
@@ -17,14 +29,14 @@ const Checkout = (props) => {
                 {item.price}
               </span>
             </div>
-            <i className="fas fa-trash-alt" />
+            <i className="fas fa-trash-alt"  onClick={() => handleDeleteItemFromCart(item.id)}/>/>
           </div>
         ))}
       </div>
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
           <h3>Precio Total:</h3>
-          <h4>$</h4>
+          <h4>${totalPrice}</h4>
         </div>
       )}
     </div>
@@ -35,6 +47,10 @@ const mapStateToProps = state => {
   return {
     cart: state.cart,
   };
+};
+
+const mapDispatchToProps = {
+  deleteItemFromCart,
 };
 
 export default connect(mapStateToProps, null)(Checkout);
