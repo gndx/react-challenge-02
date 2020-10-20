9eebccf6cbf7b4f3d9940093ac47d83bc7abe6e0
María Pérez <maria_erez@hotmail.es>
2020-10-20 17:21:32 -0500

    adding PageNotFound

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../styles/components/NotFound.styl';
+
+const NotFound = () => {
+  return <h1>No encontrado</h1>
+}
+export default NotFound;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/checkout" component={Checkout} />
+        <Route component={NotFound} />
       </Switch>
     </Layout>
   </BrowserRouter>
